9626bc71d8857c7de1388142a1d72e61d9e4df59
Vansh <vanshjain1224@gmail.com>
2020-06-23 01:37:26 +0530

    Grammatical erros in form

--- to_do/forms.py
+++ to_do/forms.py
@@ -9,25 +9,22 @@ class toDoForm(forms.ModelForm):
             'title': forms.TextInput(
 				attrs={
 					'class': 'form-control',
-                    'placeholder': 'Enter an item title'
 					}
 				),
             'description': forms.Textarea(
 				attrs={
 					'class': 'form-control',
-                    'placeholder': 'Enter an item description'
 					}
 				),
             'date_due': forms.DateInput(
     				attrs={
     					'class': 'form-control',
-                        'placeholder': 'Enter item due date (YYYY-MM-DD)'
+                        'placeholder': '(YYYY-MM-DD)'
     					}
     				),
             'category': forms.TextInput(
     				attrs={
     					'class': 'form-control',
-                        'placeholder': 'Enter an item category'
     					}
     				),
 		}
