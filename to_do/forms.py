864444a8895cbf6ca764341abc8e470f6edefe26
Vansh <vanshjain1224@gmail.com>
2020-06-22 22:07:27 +0530

    added placeholders

--- to_do/forms.py
+++ to_do/forms.py
@@ -8,22 +8,26 @@ class toDoForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(
 				attrs={
-					'class': 'form-control'
+					'class': 'form-control',
+                    'placeholder': 'Enter an item title'
 					}
 				),
             'description': forms.Textarea(
 				attrs={
-					'class': 'form-control'
+					'class': 'form-control',
+                    'placeholder': 'Enter an item description'
 					}
 				),
             'date_due': forms.DateInput(
     				attrs={
-    					'class': 'form-control'
+    					'class': 'form-control',
+                        'placeholder': 'Enter item due date YYYY-MM-DD'
     					}
     				),
             'category': forms.TextInput(
     				attrs={
-    					'class': 'form-control'
+    					'class': 'form-control',
+                        'placeholder': 'Enter an item category'
     					}
     				),
 		}
