c494fd3c243077ed0e43139e7d0492e01286d3d0
Vansh <vanshjain1224@gmail.com>
2020-06-27 01:30:31 +0530

    Added static

--- django_apps/settings.py
+++ django_apps/settings.py
@@ -129,6 +129,9 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL='/'
