216213588770eadd084f37644ada38c4bb46ec10
Vansh Jain <vanshjain1224@gmail.com>
2020-06-29 20:33:58 +0530

    Sends email on registration

--- users/views.py
+++ users/views.py
@@ -5,16 +5,27 @@ from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def register(request):
+    context = {
+        'registered': False,
+        'form': UserRegisterForm(),
+    }
     if request.method=='POST':
-        form = UserRegisterForm(request.POST)
-        if(form.is_valid()):
-            form.save()
-            return HttpResponseRedirect("/login")
-        return render(request,'users/register.html',{'form': form})
-    else:
-        form = UserRegisterForm()
+        context['form'] = UserRegisterForm(request.POST)
+        if(context['form'].is_valid()):
+            context['form'].save()
+            context['registered'] = True
+            subject = 'Welcome to ' + request.get_host()
+            body = 'Your account has been created on ' + request.get_host() + '.\n' + 'Your username: ' + context['form'].cleaned_data.get('username') + '.'
+            send_mail(
+                subject,
+                body,
+                'admin@{{request.get_host}}.com',
+                [context['form'].cleaned_data.get('email')]
+            )
 
-    return render(request,'users/register.html',{'form': form})
+    context['form'] = UserRegisterForm()
+    return render(request,'users/register.html',context)
