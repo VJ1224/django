67a81257c5268076fd0354838c14e7e902e8cefd
Vansh Jain <vanshjain1224@gmail.com>
2020-07-21 20:07:27 +0530

    Removed data from url

--- resume_builder/views.py
+++ resume_builder/views.py
@@ -11,20 +11,14 @@ def home(request):
     context = {
         'form': resumeForm()
     }
-    if request.method == 'POST':
-        form = resumeForm(request.POST)
-        context['form'] = form
-        if form.is_valid():
-            query = urlencode({'data': form.cleaned_data})
-            url = '{}?{}'.format('build/',query)
-            return HttpResponseRedirect(url)
-        return render(request, "resume_builder/resume.html", context)
     return render(request, "resume_builder/resume.html", context)
 
 def build(request):
-    data = request.GET['data']
-    resume = ast.literal_eval(data)
-    context = {
-        'resume': resume
-    }
-    return render(request, "resume_builder/build.html", context)
+    if request.method == 'POST':
+        form = resumeForm(request.POST)
+        if form.is_valid():
+            context = {
+                'resume': form.cleaned_data
+            }
+            return render(request, "resume_builder/build.html", context)
+    return HttpResponseRedirect("/resume/")
